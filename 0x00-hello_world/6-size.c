612197639718879088406a58eb28a12cc756316d
Mukwiza <itresor86@gmail.com>
2022-04-10 13:13:01 +0200

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-
 	int i;
 	double d;
 	char c;
 
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of an char is: %lu.\n", (unsigned long)sizeof(c));
 
 	return (0);
 }
