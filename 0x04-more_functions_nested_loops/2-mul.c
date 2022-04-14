ad1c3ff2094fa1c4ddf31799a6900fdb31007c18
Mukwiza <itresor86@gmail.com>
2022-04-14 12:29:13 +0200

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function that prints integers
+ * @a: to be multiplied
+ * @b: to be multiplied
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b
+	printf("%d multiplied with %d is\n", a, b, mul);
+
+	return (mul);
+}
