380c605e7d5fbc02778bd5ee975600161518852b
Mukwiza <itresor86@gmail.com>
2022-04-14 11:54:57 +0200

    more_functions_nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - evaluate function for upper case
+ * @c: print int
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this project is all about more functions nested loops
