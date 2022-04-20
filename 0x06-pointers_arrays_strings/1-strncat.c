4336a9aecda84644f0291251c92657f4cda41cd1
Mukwiza <itresor86@gmail.com>
2022-04-20 22:52:32 +0200

    pointer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ **_strncat - function that concatinate  strings
+ *@n: number of bytes src string has
+ *@dest: pointer
+ *@src: pointer
+ *Return: pointer to dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, length;
+
+	for (length = 0 ; dest[length] != '\0' ; length++)
+	{
+	}
+	for (i = 0 ; i <= n ; i++)
+	{
+		dest[length + i] = src[i];
+	}
+	return (dest);
+}
