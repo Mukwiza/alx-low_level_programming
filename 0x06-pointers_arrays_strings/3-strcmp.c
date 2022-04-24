301d9b51ea3aef4469f19489399d8cb422f2c800
Mukwiza <itresor86@gmail.com>
2022-04-24 18:32:22 +0200

    pointer

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,19 @@
  * _strcmp - function that compares strings
  * @s1: first string to compare
  * @s2: second string to compare
- * Return: 0
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, flag = 0;
 
 	for (i = 0 ; s1[i] != 0 || s2[i] ; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (2);
+			flag = 1;
 		}
 	}
-	return (0);
+	return (flag);
 }
